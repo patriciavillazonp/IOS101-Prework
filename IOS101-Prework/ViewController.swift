0d7b1c26694d1b48bae97afbe3bb0c4b2194e5b1
patriciavillazonp <101217587+patriciavillazonp@users.noreply.github.com>
2023-06-21 21:49:47 -0400

    Initial commit

---
+++ IOS101-Prework/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  IOS101-Prework
+//
+//  Created by Patricia Villazon Perez on 6/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        func changeColor() -> UIColor{
+
+                let red = CGFloat.random(in: 0...1)
+                let green = CGFloat.random(in: 0...1)
+                let blue = CGFloat.random(in: 0...1)
+
+                return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+            }
+
+    }
+    
+}
+
